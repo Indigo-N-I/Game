e859a5d05652aaf1ee98b8bf07866a70ba44081e
Tim <readatontim@gmail.com>
2021-08-20 10:46:04 -0700

    main game loop added

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
 import pygame
+import random
 pygame.init()
 
-# main game loop
+
+if __name__ == "__main__":
+    # main game loop
+    screen_width = 500
+    screen_height = 500
+
+    screen = pygame.display.set_mode((screen_width, screen_height))
+    pygame.display.set_caption("Platformer")
+
+
+
+    # game loop
+    while True:
+      for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+          pygame.quit()
+          quit()
