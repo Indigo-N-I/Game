d540b37d51cee95361dd28d3ca673cb06fe8f270
Tim <readatontim@gmail.com>
2021-08-20 11:19:41 -0700

    put testing in own file

--- enemy.py
+++ enemy.py
@@ -36,3 +36,24 @@ class Enemy(pygame.sprite.Sprite):
             self.rect.y -= self.y_speed
         elif y_dir == 1 and self.rect.y - self.y < self.dist_away:
             self.rect.y += self.y_speed
+
+if __name__ == "__main__":
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption("Enemy Testing")
+
+    all_sprites = pygame.sprite.Group()
+    e1 = Enemy(200,200)
+    all_sprites.add(e1)
+
+    screen.fill((0,0,0))
+    # game loop
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+        all_sprites.update()
+        screen.fill((0,0,0))
+        all_sprites.draw(screen)
+        pygame.display.flip()
--- platform.py
+++ platform.py
@@ -17,3 +17,24 @@ class Platform(pygame.sprite.Sprite):
 
     def update(self):
         pass
+
+if __name__ == "__main__":
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption("Platform testing")
+
+    all_sprites = pygame.sprite.Group()
+    e1 = Platform(200,200)
+    all_sprites.add(e1)
+
+    screen.fill((0,0,0))
+    # game loop
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+        all_sprites.update()
+        screen.fill((0,0,0))
+        all_sprites.draw(screen)
+        pygame.display.flip()
