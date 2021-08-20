7656044b1863e28e49741e81c8ceca9d63558ef1
Tim <readatontim@gmail.com>
2021-08-20 11:04:16 -0700

    adding platform

---
+++ platform.py
@@ -0,0 +1,13 @@
+import pygame
+pygame.init()
+
+
+class Platform(pygame.sprite.Sprite):
+    def __init__(self, x, y, w = 40, h = 10):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image = Pygame.surface((w,h))
+        self.rect = self.image.get_rect()
+
+        self.rect.x = x
+        self.rect.y = y
